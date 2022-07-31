5d127a1ebeac9a58e5e53ab03d3c6d89ec080497
Jo Minjun <alswns9288@gmail.com>
2022-07-31 23:10:10 +0900

    H2, JPA 설정 추가

--- src/main/java/minjun/ddd/OrderDeliveryApplication.java
+++ src/main/java/minjun/ddd/OrderDeliveryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrderDeliveryApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OrderDeliveryApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(OrderDeliveryApplication.class, args);
+  }
 
 }

426600a1e576f1964742dc62b225a89cd541313a
Jo Minjun <alswns9288@gmail.com>
2022-08-07 21:12:50 +0900

    change to fixture

--- src/test/java/minjun/ddd/delivery/application/port/in/DeliveryUsecaseTest.java
+++ src/test/java/minjun/ddd/delivery/application/port/in/DeliveryUsecaseTest.java
@@ -1,16 +1,12 @@
 package minjun.ddd.delivery.application.port.in;
 
+import static minjun.ddd.Fixture.createPlaceOrderCommand;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.math.BigDecimal;
-import java.util.Set;
 import minjun.ddd.delivery.domain.DeliveryStatus;
-import minjun.ddd.order.application.port.in.Address;
 import minjun.ddd.order.application.port.in.DeliveryInfo;
 import minjun.ddd.order.application.port.in.OrderDto;
-import minjun.ddd.order.application.port.in.OrderItem;
 import minjun.ddd.order.application.port.in.OrderUsecase;
-import minjun.ddd.order.application.port.in.PaymentInfo;
 import minjun.ddd.order.application.port.in.PlaceOrderCommand;
 import minjun.ddd.order.domain.state.DeliveryCompletedState;
 import minjun.ddd.order.domain.state.DeliveryStartedState;
@@ -67,48 +63,5 @@ class DeliveryUsecaseTest {
 
     assertThat(deliveryDto.getStatus()).isEqualTo(DeliveryStatus.COMPLETED.name());
     assertThat(after.getStatus()).isEqualTo(DeliveryCompletedState.class.getSimpleName());
-  }
-
-  PlaceOrderCommand createPlaceOrderCommand(Boolean isOverMinumum) {
-    return PlaceOrderCommand.builder()
-        .orderItems(createOrderItems(isOverMinumum))
-        .deliveryInfo(createDeliveryInfo(1))
-        .paymentInfo(createPaymentInfo())
-        .build();
-  }
-
-  PaymentInfo createPaymentInfo() {
-    return PaymentInfo.builder()
-        .cardNo("0000")
-        .build();
-  }
-
-  DeliveryInfo createDeliveryInfo(int i) {
-    return DeliveryInfo.builder()
-        .address(Address.builder()
-            .zipCode("대한민국 어딘가 우편번호" + i)
-            .address("대한민국 어딘가 주소" + i)
-            .build()
-        )
-        .phoneNumber("010-0000-0000")
-        .build();
-  }
-
-  Set<OrderItem> createOrderItems(Boolean isOverMinimum) {
-    if (isOverMinimum) {
-      return Set.of(createOrderItem(1L, new BigDecimal(10000L), 1));
-    }
-    return Set.of(
-        createOrderItem(1L, new BigDecimal(1000), 9),
-        createOrderItem(1L, new BigDecimal(999), 1)
-    );
-  }
-
-  OrderItem createOrderItem(Long id, BigDecimal price, Integer quantity) {
-    return OrderItem.builder()
-        .productId(id)
-        .price(price)
-        .quantity(quantity)
-        .build();
   }
 }
